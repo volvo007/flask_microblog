2ace4adc803fb0c984467543bef33bfcb218a66a
volvo007 <foxprowing@163.com>
2020-10-28 21:33:34 +0800

    Lesson 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app # 第二个 app 是 init文件里的app 对象
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world'
+    user = {'username': 'Miguel'}
+    posts = [
+            {
+                'author': {'username': 'John'},
+                'body': 'b day'
+            },
+            {
+                'author': {'username': 'Lucy'},
+                'body': 'bbb day'
+            }
+        ]
