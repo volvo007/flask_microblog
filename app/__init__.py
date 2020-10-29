fe9f81bce323acfd71c68c792ffff7c10cd45017
volvo007 <foxprowing@163.com>
2020-10-29 21:15:40 +0800

    Lesson 4

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 print('{} is using me'.format(__name__))
 
-from app import routes
+from app import routes, models
